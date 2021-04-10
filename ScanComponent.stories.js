793f536d63889759d69f9006d04f0aa827ac3d89
Neil Fraylick <29neiljf@gmail.com>
2021-04-10 14:34:30 -0400

    Add files via upload

---
+++ ScanComponent.stories.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Scan from "./ScanComponent";
+
+export const myProps = {
+  id: "1",
+  className: "mySampleClass",
+  sampleString: "Test Task",
+  sampleDate: new Date(2018, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onActionOne: action("onActionOne"),
+  onActionTwo: action("onActionTwo")
+};
+
+storiesOf("Scan", module)
+  .add("default", () => <Scan {...myProps} {...actions} />)
+  .add("pinned", () => <Scan {...myProps} pinned={true} {...actions} />)
+  .add("archived", () => <Scan {...myProps} archived={true} {...actions} />);
